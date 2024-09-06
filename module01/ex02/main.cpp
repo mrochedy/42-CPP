3176ed2c000f4cebc18e9a184540619b8acf1be9
Maxime Rochedy <mrochedy@paul-f5Ar1s1.clusters.42paris.fr>
2024-09-06 15:53:44 +0200

    Add module01 ex02

---
+++ module01/ex02/main.cpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrochedy <mrochedy@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/06 15:41:51 by mrochedy          #+#    #+#             */
+/*   Updated: 2024/09/06 15:51:52 by mrochedy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main() {
+	std::string str = "HI THIS IS BRAIN";
+	std::string *stringPTR = &str;
+	std::string &stringREF = str;
+
+	std::cout << "Address of str: " << &str << std::endl;
+    std::cout << "Address held by stringPTR: " << stringPTR << std::endl;
+    std::cout << "Address held by stringREF: " << &stringREF << std::endl << std::endl;
+
+    std::cout << "Value of str: " << str << std::endl;
+    std::cout << "Value pointed to by stringPTR: " << *stringPTR << std::endl;
+    std::cout << "Value pointed to by stringREF: " << stringREF << std::endl;
+}
