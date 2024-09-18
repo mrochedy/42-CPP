8443d5e4e5962c5b7ad3821c05bd7bf9c6e52115
mrochedy <106345479+mrochedy@users.noreply.github.com>
2024-09-18 13:12:54 +0200

    Fix float precision and negative chars that can be non displayable

--- module06/ex00/ScalarConverter.cpp
+++ module06/ex00/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: mrochedy <mrochedy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/16 17:26:31 by mrochedy          #+#    #+#             */
-/*   Updated: 2024/09/18 12:07:55 by mrochedy         ###   ########.fr       */
+/*   Updated: 2024/09/18 13:09:08 by mrochedy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,13 +60,13 @@ void fromInt(const std::string &literal) {
 
 	if (nb >= 32 && nb <= 126)
 		std::cout << WHITE << "char: " << static_cast<char>(nb) << RESET << std::endl;
-	else if (nb >= 0 && nb <= 127)
+	else if (nb >= -128 && nb <= 127)
 		std::cout << ORANGE << "char: Non displayable" << RESET << std::endl;
 	else
 		std::cout << ORANGE << "char: impossible" << RESET << std::endl;
 
 	std::cout << WHITE << "int: " << nb << RESET << std::endl;
-	std::cout << WHITE << "float: " << std::fixed << std::setprecision(1) << static_cast<float>(nb) << 'f' << RESET << std::endl;
+	std::cout << WHITE << "float: " << std::fixed << std::setprecision(1) << static_cast<double>(nb) << 'f' << RESET << std::endl;
 	std::cout << WHITE << "double: " << std::fixed << std::setprecision(1) << static_cast<double>(nb) << RESET << std::endl;
 }
 
@@ -75,7 +75,7 @@ void fromFloat(const std::string &literal) {
 
 	if (nb >= 32 && nb <= 126)
 		std::cout << WHITE << "char: " << static_cast<char>(nb) << RESET << std::endl;
-	else if (nb >= 0 && nb <= 127)
+	else if (nb >= -128 && nb <= 127)
 		std::cout << ORANGE << "char: Non displayable" << RESET << std::endl;
 	else
 		std::cout << ORANGE << "char: impossible" << RESET << std::endl;
@@ -101,7 +101,7 @@ void fromDouble(const std::string &literal) {
 
 	if (nb >= 32 && nb <= 126)
 		std::cout << WHITE << "char: " << static_cast<char>(nb) << RESET << std::endl;
-	else if (nb >= 0 && nb <= 127)
+	else if (nb >= -128 && nb <= 127)
 		std::cout << ORANGE << "char: Non displayable" << RESET << std::endl;
 	else
 		std::cout << ORANGE << "char: impossible" << RESET << std::endl;
@@ -112,7 +112,7 @@ void fromDouble(const std::string &literal) {
 		std::cout << ORANGE << "int: impossible" << RESET << std::endl;
 
 	if (nb >= -FLT_MAX && nb <= FLT_MAX)
-		std::cout << WHITE << "float: " << std::fixed << std::setprecision(1) << static_cast<float>(nb) << 'f' << RESET << std::endl;
+		std::cout << WHITE << "float: " << std::fixed << std::setprecision(1) << static_cast<double>(nb) << 'f' << RESET << std::endl;
 	else
 		std::cout << ORANGE << "float: impossible" << RESET << std::endl;
 
@@ -152,7 +152,7 @@ void ScalarConverter::convert(const std::string &literal) {
 				fromFloat(literal);
 			else
 				fromDouble(literal);
-		} else if (i == limit && dots_amount == 0) {
+		} else if (i == str_len && dots_amount == 0) {
 			fromInt(literal);
 		} else
 			std::cerr << RED << "Invalid input" << RESET << std::endl;
